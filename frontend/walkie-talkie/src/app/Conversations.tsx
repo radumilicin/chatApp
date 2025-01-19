e30026d0344ee66585c654867261d6622ae5932d
Andrei Radu Milicin <andrei.milicin@gmail.com>
2025-01-19 15:23:12 +0200

    adding groups

--- frontend/walkie-talkie/src/app/Conversations.tsx
+++ frontend/walkie-talkie/src/app/Conversations.tsx
@@ -11,6 +11,7 @@ export default function Conversations( props : any) {
     const [newChatPress, setNewChatPress] = useState(false)
     const [newGroupPress, setNewGroupPress] = useState(false)
     const [logOut, setLogOut] = useState(false)
+    const [pressed2, setPressed2] = useState(false) // this is for selecting contacts for groups
 
     useEffect(() => {
         if(props.contacts !== null) {
@@ -36,11 +37,12 @@ export default function Conversations( props : any) {
 
     return (
         <div className="relative left-[8%] w-[30%] top-[5%] h-[90%] bg-[#7DD8C3] rounded-r-xl border-white border-2">
-            {/* search bar here */}
-            <OtherOptions setMenuPress={setMenuPress} setNewChatPress={setNewChatPress}></OtherOptions>
-            <MenuDropdown menuPress={menuPress} setMenuPress={setMenuPress} onOutsideClick={setMenuPress} setNewGroupPress={setNewGroupPress} setLogOut={setLogOut}></MenuDropdown>
-            <SearchBar currentSearch={currentSearch} setCurrSearch={setCurrSearch} filterContacts={filterContacts}></SearchBar>
-            <Contacts currentSearch={currentSearch} users={props.users} filteredContacts={filteredContacts} contacts={props.contacts} curr_user={props.curr_user} images={props.images} setPressed={props.setPressed} setCurrContact={props.setCurrContact}></Contacts>
+            {newGroupPress && <GroupMaking setNewGroupPress={setNewGroupPress}></GroupMaking>}
+            {newGroupPress && <Contacts2 users={props.users} contacts={props.contacts} filteredContacts={filteredContacts} curr_user={props.curr_user} images={props.images} setNewGroupPress={setNewGroupPress} setPressed2={props.setPressed2}></Contacts2>}
+            {!newGroupPress && <OtherOptions setMenuPress={setMenuPress} setNewChatPress={setNewChatPress}></OtherOptions>}
+            {!newGroupPress && <MenuDropdown menuPress={menuPress} setMenuPress={setMenuPress} onOutsideClick={setMenuPress} setNewGroupPress={setNewGroupPress} setLogOut={setLogOut}></MenuDropdown>}
+            {!newGroupPress && <SearchBar currentSearch={currentSearch} setCurrSearch={setCurrSearch} filterContacts={filterContacts}></SearchBar>}
+            {!newGroupPress && <Contacts currentSearch={currentSearch} users={props.users} filteredContacts={filteredContacts} contacts={props.contacts} curr_user={props.curr_user} images={props.images} setPressed={props.setPressed} setCurrContact={props.setCurrContact}></Contacts>}
         </div>
     );
 }
@@ -81,8 +83,9 @@ export function MenuDropdown (props) {
 
 export function OtherOptions (props) {
     return (
-        <div className="absolute left-[2%] top-[1%] h-[5%] w-[98%] flex flex-col">
-            <div className="relative left-[78%] w-[20%] h-full flex flex-row items-center">
+        <div className="absolute left-[2%] top-[1%] h-[5%] w-[98%] flex flex-row">
+            <div className="relative indent-[20px] left-[2%] w-[10%] text-xl font-semibold text-black font-sans flex flex-row justify-center items-center">Chats</div>
+            <div className="relative left-[66%] w-[20%] h-full flex flex-row items-center">
                 <div className="relative left-0 w-[50%] h-full hover:bg-slate-400 hover:rounded-xl flex flex-row items-center justify-center" onClick={() => {props.setNewChatPress(true)}}>
                     <img src="./newChat2.png" className="justify-end items-center max-h-[80%] max-w-[100%]"></img>
                 </div>
@@ -264,6 +267,183 @@ export function Contacts( props: any) {
                                             ? "Sent " + getLastMessage(element, idx).timestamp.split("T")[1].split(".")[0].slice(0, 5)
                                             : getLastMessage(element, idx).timestamp.split("T")[1].split(".")[0].slice(0, 5)
                                         }
+                                    </div>
+
+                                </div>
+                        </div>
+                        </div>
+                    </div> : <></>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export function GroupMaking(props) {
+
+    const [usernameSearch, setUsernameSearch] = useState('')
+
+    return (
+        <div className="absolute left-0 top-0 w-full h-[15%]">
+            <div className="relative flex flex-row h-[55%] w-full">
+                <div className="relative flex flex-col justify-center items-center left-2 w-[15%] top-[15%] h-[70%] text-xl font-semibold text-black hover:bg-slate-500 rounded-md p-2" onClick={() => {props.setNewGroupPress(false)}}>
+                    <img src="./xicon.png" className="w-[60%] h-full"></img> 
+                </div> 
+                <div className="flex w-[20%] h-full text-xl font-semibold flex-col justify-center items-center text-black font-sans">Group</div>
+            </div>
+            <div className="relative top-[10%] flex flex-row left-[5%] w-[75%] h-[35%] border-b-2 border-gray-500 z-20">
+                <input placeholder="Search name" value={usernameSearch} className="left-[5%] w-[90%] h-full outline-none bg-transparent overflow-x-auto" 
+                    onChange={(e) => {setUsernameSearch(e.target.value)}}></input>
+            </div>
+        </div>
+    );
+}
+
+export function Contacts2( props: any) {
+
+    let curr_user = 1
+
+    const isBase64 = value => /^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{4})$/.test(value);
+
+    // here I need to have current user .. so then I can extract its contacts .. 
+    // let's say for simplicity curr_user = 1
+    // and here we extract its contacts and the messages that happen most recently are shown first
+
+    function getNameWithUserId(contact: any) {
+        const user = props.users.find((user) => user.id === contact.contact_id);
+        return user ? user.username : "";
+    }
+
+    function getUser(contact: any) {
+        return props.users.find((user) => user.id === contact.contact_id);
+    }
+     
+    function getLastMessage(contact : any, idx: number) {
+        let lenMsgs = contact.message.length
+        let last_msg = contact.message[lenMsgs - 1]
+        console.log(`this is the ${idx}th contact in the list with the message = ${last_msg.message}`)
+        return last_msg
+    }
+
+    function getUnreadMessages(contact: any, idx: number) {
+        let lenMsgs = contact.message.length
+        let last_msg = contact.message[lenMsgs - 1]
+        let cnt_unread = 0
+        if(last_msg.recipient_id === curr_user) {
+            for(let i = lenMsgs - 1; i > -1 ; i--){
+                if(contact.message[i].recipient_id === curr_user) cnt_unread += 1
+                else break
+            }
+        }
+        return cnt_unread
+    }
+
+    function getImage(contact: any) {
+        const image = props.images.find((image: any) => image.user_id === contact.contact_id);
+        return image || { data: "" }; // Ensure we return a fallback value
+    }
+
+    // type user is either current or other (0,1)
+    function getProfileImage(contact: any, type_user : number) {
+        const user = props.users.find((user) => {
+            if(type_user === 0){
+                return user.id === props.curr_user
+            } else {
+                return contact.contact_id === user.id
+            }
+        })
+        const image = props.images.find((image: any) => image.id === user.image_id);
+        return image || { data: "" }; // Ensure we return a fallback value
+    }
+
+    return (
+        <div className="absolute left-0 top-[16%] w-full h-[84%]">
+            <div className="relative top-0 left-0 h-full w-full flex flex-col overflow-scroll">
+                { props.filteredContacts !== null && props.filteredContacts.map((element: any, idx: number) => (
+                    element.id === props.curr_user ?
+                    <div
+                        key={idx}
+                        className={`relative h-[12%] w-full bg-slate-400 bg-opacity-50 flex flex-row border-y-gray-700 border-t-[1px]`}
+                        onClick={() => {props.setPressed2(element); }}// here add to the list of contacts console.log("clicked")}}
+                    >
+                        <div className="flex w-[10%] justify-center items-center">
+                            {/* Use base64 data for image */}
+                            {getImage(element).data !== "" ? <img
+                                src={`data:image/jpg;base64,${getImage(element).data}`}
+                                className="h-[75%] w-[75%] rounded-full"
+                                alt="Profile"
+                            /> : getProfileImage(element, 1).data !== "" ? <img
+                                src={`data:image/jpg;base64,${getImage(element).data}`}
+                                className="h-[75%] w-[75%] rounded-full"
+                                alt="Profile"></img> : 
+                                <img src="./userProfile.jpg" className="h-[75%] w-[75%] rounded-full"></img>}
+                        </div>
+                        <div className="flex w-[90%] flex-col">
+                            <div className="flex h-[50%] w-full items-center flex-row">
+                                <div className="w-[80%] h-full">
+                                    <div className="indent-[20px] text-xl font-medium font-sans">
+                                        {getNameWithUserId(element)}
+                                    </div>
+                                </div>
+                                <div className="w-[20%] h-full flex flex-row justify-center">
+                                    <div className="rounded-full contain-size bg-green-700 justify-center bg-contain h-full text-sm">
+                                        {element.message[0] !== null && element.message[0].recipient_id === curr_user && getUser(element).about}
+                                    </div> 
+                                </div>
+                            </div>
+                            <div className="relative flex w-full h-[50%] items-center">
+                                {/* Left text container */}
+                                <div className="relative flex flex-row h-full w-[80%]">
+                                    <div className="indent-[20px] flex h-full w-full items-center text-sm text-gray-600 font-medium overflow-x-hidden">
+                                        {getUser(element).about}
+                                    </div>
+                                </div>
+                                {/* Right time container */}
+                                <div className="relative flex flex-row h-full w-[20%]">
+                                    <div className="flex h-full w-full flex-row items-center text-sm text-gray-600 font-medium">
+                                    </div>
+                                </div>
+                        </div>
+                        </div>
+                    </div> : <></>))}
+                { props.filteredContacts === null && props.contacts.map((element: any, idx: number) => (
+                    element.id === props.curr_user ?
+                    <div
+                        key={idx}
+                        className={`relative h-[12%] w-full bg-slate-400 bg-opacity-50 flex flex-row border-y-black border-2`}
+                        onClick={() => {props.setPressed(element); props.setCurrContact(element); console.log("clicked")}}
+                    >
+                        <div className="flex w-[10%] justify-center items-center">
+                            {/* Use base64 data for image */}
+                            <img
+                                src={`data:image/jpg;base64,${getImage(element).data}`}
+                                className="h-[75%] w-[75%] rounded-full"
+                                alt="Profile"
+                            />
+                        </div>
+                        <div className="flex w-[90%] flex-col">
+                            <div className="flex h-[50%] w-full items-center flex-row">
+                                <div className="w-[80%] h-full">
+                                    <div className="indent-[20px] text-xl font-medium font-sans">
+                                        {getNameWithUserId(element)}
+                                    </div>
+                                </div>
+                                <div className="w-[20%] h-full flex flex-row justify-center">
+                                    <div className="rounded-full contain-size bg-green-700 justify-center bg-contain h-full">
+                                        {getUser(element).about}
+                                    </div> 
+                                </div>
+                            </div>
+                            <div className="relative flex w-full h-[50%] items-center">
+                                {/* Left text container */}
+                                <div className="relative flex flex-row h-full w-[80%]">
+                                    <div className="indent-[20px] flex h-full w-full items-center text-sm text-gray-600 font-medium">
+                                        {getLastMessage(element, idx).message}
+                                    </div>
+                                </div>
+                                {/* Right time container */}
+                                <div className="relative flex flex-row h-full w-[20%]">
+                                    <div className="flex h-full w-full flex-row items-center text-sm text-gray-600 font-medium">
                                     </div>
 
                                 </div>
